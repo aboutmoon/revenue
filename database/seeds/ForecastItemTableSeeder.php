26a27fba7b5b05e915d7e4c26a2a9b75445f3213
254543199@qq.com <254543199@qq.com>
2019-11-22 16:57:13 +0800

    full tables

--- database/seeds/ForecastItemTableSeeder.php
+++ database/seeds/ForecastItemTableSeeder.php
@@ -12,13 +12,6 @@ class ForecastItemTableSeeder extends Seeder
      */
     public function run()
     {
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 7, 'fi_account_id' => 7, 'fi_item_id' => 1, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 8, 'fi_account_id' => 7, 'fi_item_id' => 1, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 9, 'fi_account_id' => 7, 'fi_item_id' => 1, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 10, 'fi_account_id' => 5, 'fi_item_id' => 1, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 10, 'fi_account_id' => 6, 'fi_item_id' => 1, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 10, 'fi_account_id' => 9, 'fi_item_id' => 1, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 10, 'fi_account_id' => 9, 'fi_item_id' => 2, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
-        ForecastItem::create(['model_id' => 1, 'model_vid' => 1, 'fi_location_id' => 10, 'fi_account_id' => 9, 'fi_item_id' => 3, 'date_from_id' => 1, 'date_to_id' => 1, 'coverage' => 14543.234]);
+
     }
 }
