2eff76187e2855508215f3013ceec2d6533dfac6
254543199@qq.com <254543199@qq.com>
2019-12-12 10:35:54 +0800

    horizon

--- resources/views/model-results/index.blade.php
+++ resources/views/model-results/index.blade.php
@@ -24,7 +24,7 @@
                 <!-- /.card-header -->
                 <div class="card-body table-responsive p-0">
                     <div id="output"></div>
-                    <table class="table table-hover " id="model-result">
+                    <table class="table table-hover " id="model-result" style="display: none">
                         <thead>
                         <tr>
                             <th>Market</th>
